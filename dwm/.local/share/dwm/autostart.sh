77dcf1501d52a8cb143722be02474bd041b1c8f1
Remi Labbe <labbe.remi@free.fr>
2022-01-25 16:45:57 +0100

    update picom nm and add dunst

--- dwm/.local/share/dwm/autostart.sh
+++ dwm/.local/share/dwm/autostart.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # xrdb $XDG_CONFIG_HOME/X/Xresources &
+nm-applet --indicator &
 dwmblocks &
-picom &
+picom -b
 lxsession &
 feh --no-fehbg --bg-center $XDG_CONFIG_HOME/wall.jpg &
+dunst &
