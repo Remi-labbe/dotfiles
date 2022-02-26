1f97648186cd7ec13618855915ae6b84216ad353
Remi Labbe <labbe.remi@free.fr>
2022-02-26 21:32:39 +0100

    asm indent style

--- nvim/.config/nvim/lua/user/autocommands.lua
+++ nvim/.config/nvim/lua/user/autocommands.lua
@@ -1,10 +1,14 @@
 vim.cmd [[
   augroup _general_settings
     autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200}) 
+    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
+    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200})
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
+  augroup end
+  augroup _asm
+    autocmd!
+    autocmd FileType asm setlocal noexpandtab
   augroup end
   augroup _git
     autocmd!
