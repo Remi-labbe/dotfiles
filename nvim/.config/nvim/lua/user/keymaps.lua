80242872fcfb9cf6f14a180cf83a14e4f918faef
Remi Labbe <labbe.remi@free.fr>
2022-06-16 16:25:40 +0200

    truezen keymap

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -67,3 +67,6 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
 
+-- Zen mode
+keymap("n", "<leader>tz", ":TZAtaraxis<CR>", opts)
+
