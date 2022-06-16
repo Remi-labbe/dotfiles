1f1f4380cd5db1f9b8a9d99ffe5fa7f9cb274bbc
Remi Labbe <labbe.remi@free.fr>
2022-06-16 16:33:01 +0200

    reformat

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -1,41 +1,41 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-	return
+  return
 end
 
 local hide_in_width = function()
-	return vim.fn.winwidth(0) > 80
+  return vim.fn.winwidth(0) > 80
 end
 
 local diagnostics = {
-	"diagnostics",
-	sources = { "nvim_diagnostic"},
-	sections = { "error", "warn", "info", "hint" },
-	symbols = { error = " ", warn = " ", info = " ", hint = " " },
-	colored = false,
-	update_in_insert = false,
-	always_visible = false,
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn", "info", "hint" },
+  symbols = { error = " ", warn = " ", info = " ", hint = " " },
+  colored = true,
+  update_in_insert = false,
+  always_visible = false,
 }
 
 local diff = {
-	"diff",
-	colored = false,
-	symbols = { added = "[+]", modified = "[~]", removed = "[-]" }, -- changes diff symbols
+  "diff",
+  colored = false,
+  symbols = { added = "+", modified = "~", removed = "-" }, -- changes diff symbols
   cond = hide_in_width
 }
 
 local filename = {
   "filename",
-  file_status = true,      -- Displays file status (readonly status, modified status)
-  path = 1,                -- 0: Just the filename
-                           -- 1: Relative path
-                           -- 2: Absolute path
+  file_status = true, -- Displays file status (readonly status, modified status)
+  path = 1, -- 0: Just the filename
+  -- 1: Relative path
+  -- 2: Absolute path
 
-  shorting_target = 30,    -- Shortens path to leave 40 spaces in the window
-                           -- for other components. (terrible name, any suggestions?)
+  shorting_target = 30, -- Shortens path to leave 40 spaces in the window
+  -- for other components. (terrible name, any suggestions?)
   symbols = {
-    modified = ' [+]',      -- Text to show when the file is modified.
-    readonly = ' [RO]',      -- Text to show when the file is non-modifiable or readonly.
+    modified = ' [+]', -- Text to show when the file is modified.
+    readonly = ' [RO]', -- Text to show when the file is non-modifiable or readonly.
     unnamed = '[No Name]', -- Text to show for unnamed buffers.
   }
 }
@@ -47,14 +47,14 @@ local filename = {
 -- }
 
 local branch = {
-	"branch",
-	icons_enabled = true,
-	icon = "",
+  "branch",
+  icons_enabled = true,
+  icon = "",
 }
 
 local location = {
-	"location",
-	padding = 0,
+  "location",
+  padding = 0,
 }
 
 -- lualine_a = { branch, diagnostics },
@@ -69,9 +69,9 @@ lualine.setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-		disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
     always_divide_middle = true,
   },
   sections = {
