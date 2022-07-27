0a9819b3a6052119e7c2d97b75f784a4b55fc573
Remi Labbe <labbe.remi@free.fr>
2022-07-27 14:22:10 +0200

    formatted

--- nvim/.config/nvim/lua/user/sets.lua
+++ nvim/.config/nvim/lua/user/sets.lua
@@ -1,56 +1,56 @@
 local options = {
-  -- change window title from terminal name to actual file
-  title = true,
+    -- change window title from terminal name to actual file
+    title = true,
 
-  -- current line number + relatives numbers is the best
-  number = true,
-  relativenumber = true,
+    -- current line number + relatives numbers is the best
+    number = true,
+    relativenumber = true,
 
-  -- smart tabs
-  expandtab = true,
-  shiftwidth = 4,
-  tabstop = 4,
-  softtabstop = 4,
-  shiftround = true,
+    -- smart tabs
+    expandtab = true,
+    shiftwidth = 4,
+    tabstop = 4,
+    softtabstop = 4,
+    shiftround = true,
 
-  -- Make it fast
-  updatetime = 50,
+    -- Make it fast
+    updatetime = 50,
 
-  -- use system clipboard for y p
-  clipboard = "unnamedplus",
-  backup = false,                          -- creates a backup file
-  cmdheight = 1,                           -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  conceallevel = 0,                        -- so that `` is visible in markdown files
-  fileencoding = "utf-8",                  -- the encoding written to a file
-  hlsearch = false,                        -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
-  mouse = "a",                             -- allow the mouse to be used in neovim
-  pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 2,                         -- always show tabs
-  smartcase = true,                        -- smart case
-  smartindent = true,                      -- make indenting smarter again
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  swapfile = false,                        -- creates a swapfile
-  termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 400,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true,                         -- enable persistent undo
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  cursorline = true,                       -- highlight the current line
-  numberwidth = 2,                         -- set number column width to 2 {default 4}
-  signcolumn = "yes",
-  colorcolumn = "80",
-  wrap = false,
-  scrolloff = 8,
-  sidescrolloff = 8,
+    -- use system clipboard for y p
+    clipboard = "unnamedplus",
+    backup = false, -- creates a backup file
+    cmdheight = 1, -- more space in the neovim command line for displaying messages
+    completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+    conceallevel = 0, -- so that `` is visible in markdown files
+    fileencoding = "utf-8", -- the encoding written to a file
+    hlsearch = false, -- highlight all matches on previous search pattern
+    ignorecase = true, -- ignore case in search patterns
+    mouse = "a", -- allow the mouse to be used in neovim
+    pumheight = 10, -- pop up menu height
+    showmode = false, -- we don't need to see things like -- INSERT -- anymore
+    showtabline = 2, -- always show tabs
+    smartcase = true, -- smart case
+    smartindent = true, -- make indenting smarter again
+    splitbelow = true, -- force all horizontal splits to go below current window
+    splitright = true, -- force all vertical splits to go to the right of current window
+    swapfile = false, -- creates a swapfile
+    termguicolors = true, -- set term gui colors (most terminals support this)
+    timeoutlen = 400, -- time to wait for a mapped sequence to complete (in milliseconds)
+    undofile = true, -- enable persistent undo
+    writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+    cursorline = true, -- highlight the current line
+    numberwidth = 2, -- set number column width to 2 {default 4}
+    signcolumn = "yes",
+    colorcolumn = "80",
+    wrap = false,
+    scrolloff = 8,
+    sidescrolloff = 8,
 }
 
 vim.opt.shortmess:append "c"
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+    vim.opt[k] = v
 end
 
 vim.g.netrw_banner = 0
