712089435e01c8e8fc338aa635557cb8ada93da0
Remi Labbe <labbe.remi@free.fr>
2022-07-28 10:10:42 +0200

    format config

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -18,11 +18,19 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key(
+        [mod],
+        "space",
+        lazy.layout.next(),
+        desc="Move window focus to other window",
+    ),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key(
-        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+        [mod, "shift"],
+        "h",
+        lazy.layout.shuffle_left(),
+        desc="Move window to the left",
     ),
     Key(
         [mod, "shift"],
@@ -30,15 +38,30 @@ keys = [
         lazy.layout.shuffle_right(),
         desc="Move window to the right",
     ),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key(
+        [mod, "shift"],
+        "j",
+        lazy.layout.shuffle_down(),
+        desc="Move window down",
+    ),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key(
-        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+        [mod, "control"],
+        "h",
+        lazy.layout.grow_left(),
+        desc="Grow window to the left",
     ),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key(
+        [mod, "control"],
+        "l",
+        lazy.layout.grow_right(),
+        desc="Grow window to the right",
+    ),
+    Key(
+        [mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"
+    ),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
@@ -51,13 +74,19 @@ keys = [
         desc="Kill focused window",
     ),
     Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "shift"], "q", lazy.spawn("dm_sysact"), desc="Reload the config"),
+    Key(
+        [mod, "shift"], "q", lazy.spawn("dm_sysact"), desc="Reload the config"
+    ),
     Key([mod], "p", lazy.spawn(dmenu_run), desc="Run dmenu"),
     Key([mod], "backslash", lazy.spawn("dm_maim"), desc="Run dmenu"),
     # SOUND
     Key([], "XF86AudioMute", lazy.spawn("pamixer -t"), desc="mute sound"),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("pamixer -i 5"), desc="volume +"),
-    Key([], "XF86AudioLowerVolume", lazy.spawn("pamixer -d 5"), desc="volume -"),
+    Key(
+        [], "XF86AudioRaiseVolume", lazy.spawn("pamixer -i 5"), desc="volume +"
+    ),
+    Key(
+        [], "XF86AudioLowerVolume", lazy.spawn("pamixer -d 5"), desc="volume -"
+    ),
     Key(
         [],
         "XF86AudioMicMute",
@@ -122,7 +151,6 @@ TokyoNight = dict(
 )
 
 COLORS = TokyoNight
-
 
 layout_theme = {
     "border_width": 1,
@@ -217,7 +245,10 @@ mouse = [
         start=lazy.window.get_position(),
     ),
     Drag(
-        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+        [mod],
+        "Button3",
+        lazy.window.set_size_floating(),
+        start=lazy.window.get_size(),
     ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
